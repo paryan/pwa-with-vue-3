10905254105732ba0cedd119b86017637418d7bc
Piyush Aryan <>
2022-02-05 17:50:01 -0800

    Initial commit

---
+++ dist/precache-manifest.082b49adcafec8874b72c7f67ab72d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befb21e5ffb4aa93722f",
+    "url": "/css/app.3849fcfe.css"
+  },
+  {
+    "revision": "87ea7f1409daa1583ce7b5fe1e5f31d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "befb21e5ffb4aa93722f",
+    "url": "/js/app.4406f3f2.js"
+  },
+  {
+    "revision": "3104b415364e3256f230",
+    "url": "/js/chunk-vendors.64b9cd16.js"
+  },
+  {
+    "revision": "e34d9ff141e75733a5e8d25e0f8aa9ae",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
